051364a75c029e81623fc660c1788f706d605a90
Roberto Solares <betosolaresgar@gmail.com>
2019-11-08 18:40:32 -0600

    Add model for the pizzas

---
+++ pizza-api/Models/Pizza.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace pizza_api.Models
+{
+    public class Pizza
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<string> Ingredients { get; set; }
+        public string Size { get; set; }
+        public int Slices { get; set; }
+        public bool HasExtraCheese { get; set; }
+    }
+}
