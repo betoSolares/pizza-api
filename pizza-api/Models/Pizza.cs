9a262a1d573c9a90c0db89ab4b5c5c25669e491a
Roberto Solares <betosolaresgar@gmail.com>
2019-11-10 12:00:15 -0600

    Add a method to check when two pizzas are equals

--- pizza-api/Models/Pizza.cs
+++ pizza-api/Models/Pizza.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace pizza_api.Models
 {
-    public class Pizza
+    public class Pizza : IEquatable<Pizza>
     {
         public string Name { get; set; }
         public string Description { get; set; }
@@ -10,5 +11,10 @@ namespace pizza_api.Models
         public string Size { get; set; }
         public int Slices { get; set; }
         public bool HasExtraCheese { get; set; }
+
+        public bool Equals(Pizza other)
+        {
+            return Name.Equals(other.Name);
+        }
     }
 }
