095a4de784d5c0f8ba68a2c5f1b7d730437266c9
Roberto Solares <betosolaresgar@gmail.com>
2019-11-11 14:50:41 -0600

    Create the class to store the db settings

---
+++ pizza-api/Services/PizzasDatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace pizza_api.Services
+{
+    public class PizzasDatabaseSettings
+    {
+        public string CollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
